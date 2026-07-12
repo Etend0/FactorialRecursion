6e1c1e9059a87c1db2834cdbfb8d505e25c54483
Etendo <etendo123@gmail.com>
2026-07-12 03:21:32 -0600

    Added a Utility class to read user input and call the factorial methods.

--- FactorialRecursion/Program.cs
+++ FactorialRecursion/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Elijah Hodge
+ * CST - 250
+ * 03/16/2026
+ * Factorial Recursion
+ * Activity 3
+ */
+
+//--------------------------------------------------------
+// Start of the Main Method
+//--------------------------------------------------------
+
+// Declare and initialize
+using FactorialRecursion.Services.BusinessLogicLayer;
+using System.Numerics;
+
+FactorialLogic factorialLogic = new FactorialLogic();
+int input = 0;
+BigInteger iterativeAns = 0, recursiveAns = 0;
+
+// Prompt the user
+Console.Write("Enter a positive number: ");
+// Get the users input
+input = Utility.ReadIntFromConsole();
+
+// Solve the factorial using iteration
+Console.WriteLine("Solving the factorial using iteration...");
+iterativeAns = factorialLogic.SolveIterativeFactorial(input);
+Console.WriteLine($"Answer: {iterativeAns}");
+
+// Solve the factorial using recursion
+Console.WriteLine("Solving the factorial using recursion...");
+recursiveAns = factorialLogic.SolveRecursiveFactorial(input);
+Console.WriteLine($"Answer: {recursiveAns}");
+
+//--------------------------------------------------------
+// End of the Main Method
+//--------------------------------------------------------
+static class Utility
+{
+    /// <summary>
+    /// Read integer from the console
+    /// </summary>
+    /// <returns></returns>
+    internal static int ReadIntFromConsole()
+    {
+        // Declare and initialize
+        string input = "";
+        int number = -1;
+
+        // Get the current input
+        input = Console.ReadLine();
+        // Check if the input is valid
+        while (!int.TryParse(input, out number))
+        {
+            // Show the user an error message and prompt them again
+            Console.Write("Invalid input. Please try again: ");
+            // Get the new input
+            input = Console.ReadLine();
+        }
+        // Return the resulting integer from the user
+        return number;
+    }
+}
