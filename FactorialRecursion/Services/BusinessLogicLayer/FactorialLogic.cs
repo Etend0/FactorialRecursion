9961af650a5ccccd4495f3ee4b6bdd191719c7e8
Etendo <etendo123@gmail.com>
2026-07-12 02:13:03 -0600

    Added logic to solve the factorial problem using iteration or recursion.

---
+++ FactorialRecursion/Services/BusinessLogicLayer/FactorialLogic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialRecursion.Services.BusinessLogicLayer
+{
+    internal class FactorialLogic
+    {
+        /// <summary>
+        /// Solve the factorial problem using iteration
+        /// </summary>
+        /// <param name="factorial"></param>
+        /// <returns></returns>
+        internal BigInteger SolveIterativeFactorial(int factorial)
+        {
+            // Declare and initialize
+            BigInteger result = 1;
+
+            // For loop to solve the factorial recursion
+            for (int i = factorial; i >= 1; i--)
+            {
+                // Multiply the result by i
+                result *= i;
+            }
+            // Return the result
+            return result;
+        }
+
+        /// <summary>
+        /// Solve the factorial problem using recursion
+        /// </summary>
+        /// <param name="factorial"></param>
+        /// <returns></returns>
+        internal BigInteger SolveRecursiveFactorial(int factorial)
+        {
+            // Big case: factorial is 0 or 1
+            if (factorial == 0 || factorial == 1)
+            {
+                return 1;
+            }
+            // Perform the recursive
+            return factorial * SolveRecursiveFactorial(factorial - 1);
+        }
+    }
+}
